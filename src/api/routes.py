6fb88eaae2a57f4246af1d3863ac14a49003b582
scentmay <scentmay@gmail.com>
2022-08-08 19:41:04 +0000

    métodos get, post y busqueda por id funcionando ok

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,6 +7,28 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+#Consulta de todos los usuarios
+@api.route('/users', methods=['GET'])
+def funcionPrueba():
+    people_query = User.query.all()
+    all_people = list(map(lambda x: x.serialize(), people_query))
+    return jsonify(all_people)
+
+# Búsqueda de un usuario por ID
+@api.route('/search/<int:id>', methods=['GET'])
+def getUser(id):
+    people_query = User.query.get(id)
+    return jsonify(people_query.serialize())
+
+# Introducción de usuario en la tabla
+@api.route('/register', methods=['POST'])
+def createUser():
+    info_request = request.get_json()
+    newUser = User(id = info_request['id'], email = info_request['email'], password = info_request['password'], is_active = info_request['is_active'])
+    db.session.add(newUser)
+    db.session.commit()
+    return "Usuario creado", 201
+
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
